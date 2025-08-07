f99ce4539d768a3481abe007a01760ac3c36c861
Just-In <justin.c.aquino14@gmail.com>
2025-08-07 14:59:32 -0400

    workiing music, file name changes, pop up box or menu screen alost complete

---
+++ Javascript/Musicplayer.js
@@ -0,0 +1,7 @@
+document.getElementById('Menu').addEventListener('click', function () {
+    // Hide the menu
+    this.style.display = 'none';
+    // Play the music (in case autoplay was blocked)
+    const audio = document.getElementById('background-music');
+    audio.play();
+});
